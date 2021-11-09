94f5c6ada8db3dcc38076423a71058fca8610312
Omar Ayman <omarayman.dev@gmail.com>
2021-11-10 01:16:34 +0200

    IsConnected Commit

--- app/src/main/java/com/example/mystore/StartActivity.java
+++ app/src/main/java/com/example/mystore/StartActivity.java
@@ -2,8 +2,12 @@ package com.example.mystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,25 +20,43 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         auth=FirebaseAuth.getInstance();
+        if (isConnected()){
         if(auth.getCurrentUser()!=null){
             Toast.makeText(this, "Please wait", Toast.LENGTH_SHORT).show();
             Intent intent=new Intent(this,MainActivity.class);
             startActivity(intent);
             Toast.makeText(this, "You're Already Logged In", Toast.LENGTH_SHORT).show();
 
-        }
+        }}else Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_LONG).show();
     }
 
     public void OnStartActivity(View view) {
         switch (view.getId()){
             case R.id.to_LoginLayout:
-                Intent intent= new Intent(this,LoginActivity.class);
-                startActivity(intent);
+                if (isConnected()){
+                        Intent intent= new Intent(this,LoginActivity.class);
+                        startActivity(intent);
+                    }else Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_LONG).show();
                 break;
             case R.id.to_signupLayout:
-                Intent intent2= new Intent(this,SignUpActivity.class);
-                startActivity(intent2);
+                if (isConnected()){
+                    Intent intent2= new Intent(this,SignUpActivity.class);
+                    startActivity(intent2);
+                }else Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_SHORT).show();
+
                 break;
         }
+    }
+    public boolean isConnected() {
+        boolean connection = false;
+        try {
+            ConnectivityManager cm = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo nInfo = cm.getActiveNetworkInfo();
+            connection = nInfo != null && nInfo.isAvailable() && nInfo.isConnected();
+            return connection;
+        } catch (Exception e) {
+            Log.e("Connectivity Exception", e.getMessage());
+        }
+        return connection;
     }
 }
